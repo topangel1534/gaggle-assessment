66510d8c31e02f7d20c8be087b76e16bca85ad9b
devmachin <matheusangel534@gmail.com>
2022-11-12 07:38:11 -0500

    Add signup dialog component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,55 @@
+import { useState } from 'react';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+
+import SignUp from 'components/SignUp';
+
 import './index.scss';
 
 // eslint-disable-next-line import/no-anonymous-default-export
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: '#fff',
+  boxShadow: 24,
+  borderRadius: '3px',
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="footer">
-      <div className="register">Register</div>
+      <div className="register" onClick={handleOpen}>
+        Register
+      </div>
       <div className="vertical-line"></div>
       <div className="forgot-password">Forgot password?</div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <SignUp />
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 };
--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,6 +1,23 @@
+import TextField from 'components/TextField';
 import './index.scss';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  return <div className="sign-up"></div>;
+  return (
+    <div className="sign-up">
+      <div className="sign-up-header">
+        <div className="sign-up-left-section">🐣</div>
+        <div className="sign-up-control">
+          <p className="caption">New Account</p>
+          <TextField label="Username" />
+          <TextField label="Password" option="Hide" />
+          <TextField label="Email" />
+        </div>
+      </div>
+      <div className="sign-up-footer">
+        <div>Cancel</div>
+        <div>Register Account</div>
+      </div>
+    </div>
+  );
 };
