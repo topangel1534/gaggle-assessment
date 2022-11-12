9443d9a895c4d71ec9238c4c3509d52a17213887
devmachin <matheusangel534@gmail.com>
2022-11-12 06:57:53 -0500

    Add login component

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,4 +1,8 @@
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import TextField from 'components/TextField';
+
 import './index.scss';
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -7,6 +11,13 @@ export default () => {
     <div className="login">
       <TextField label="Username" />
       <TextField label="Password" option="Hide" />
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="Remember"
+        />
+      </FormGroup>
+      <div className="sign-on-button">Sign On</div>
     </div>
   );
 };
